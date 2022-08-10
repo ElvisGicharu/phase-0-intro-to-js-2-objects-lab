04297dc86be43d7b085bbd8f35c5efbed9d5b7f9
elvis <gicharuelvis@gmail.com>
2022-08-10 23:08:49 +0300

    commit tree 28eccd28951e3ab7c6d860357bee88ddc83f7354

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+const employee= {name:'Elvis',streetAddress:'Kanairo'};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){ 
+    // return Object.assign({},employee,{ [key]:value});
+    return{...employee,[key]:value};
+};
+
+// function data (name,work,experience){
+//     return {...employee,name,work,experience}
+//   }
+//   console.log(data('James','Analyst',1));
+//   console.log(employee);
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+
+//   function destructivelyUpdateEmployeeWithKeyAndValue(employee,name,streetAddress){
+//     employee= {name,streetAddress};
+//     return employee;
+// }
+// console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee,'Jane','Choka'));
+
+// console.log(employee);
+
+
+function deleteFromEmployeeByKey(employee,name){
+    const newEmployee={...employee,name}
+    delete newEmployee[name];
+    return  newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,name){
+    delete employee.name;
+    return employee;
+}
+
